61b3c5f86d1e474a1a2482f2beb5b06b1b17a9cc
HashtagOrNah <drghostlly@gmail.com>
2022-04-13 01:51:25 -0700

    changed target to the flask host ip (hopefully its universal) and made vue target /api instead of /ws

--- ui/vue.config.js
+++ ui/vue.config.js
@@ -2,7 +2,11 @@ const {defineConfig} = require('@vue/cli-service')
 module.exports = defineConfig({
     transpileDependencies: true,
     devServer: {
-        proxy: 'http://localhost:5000/',
+        proxy: {
+            '^/api': {
+                target: 'http://127.0.0.1:5000'
+            }
+        },
     },
 
     pluginOptions: {
