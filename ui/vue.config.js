4fbe697b0f02ef1fca91ffb4c973230e49b7f06b
Daniel Lee <daniel@devshell.net>
2022-04-24 22:26:46 -0700

    move files around, add docker

--- ui/vue.config.js
+++ ui/vue.config.js
@@ -4,7 +4,7 @@ module.exports = defineConfig({
     devServer: {
         proxy: {
             '^/api': {
-                target: 'http://127.0.0.1:5000'
+                target: 'https://api.sgse.one'
             }
         },
     },
